f71440b6a49fa33c6c4a97d03521a9193a0e32f6
joh-maker <80920155+joh-maker@users.noreply.github.com>
2021-03-30 13:37:52 +0530

    hi

---
+++ data.php
@@ -0,0 +1,40 @@
+<?php
+$num1="|1|";
+$num11="                              English iii|";
+$num111="      MS.Lapen|";
+$num2="|2|";
+$num22="                              precalculus|";
+$num222="    MRS.Gideon|";
+$num3="|3|";
+$num33="                             Music Theory|";
+$num333="      mr.Davis|";
+$num4="|4|";
+$num44="                            Biotechnology|";
+$num444="     MS.Palmer|";
+$num5="|5|";
+$num55="               Principals of technology i|";
+$num555="     MS.Garcia|";
+$num6="|6|";  
+$num66="                                 Latin ii|";
+$num666="   MRS.Barnett|";
+$num7="|7|";
+$num77="                            AP US History|";
+$num777="MR.Johannessan|";
+$num8="|8|";
+$num88="Business communication information system|";
+$num888="      MR.James|";          
+echo "+------------------------------------------------------------+\n";
+echo "$num1 $num11 $num111\n";
+echo "$num2 $num22 $num222\n";
+echo "$num3 $num33 $num333\n";
+echo "$num4 $num44 $num444\n";
+echo "$num5 $num55 $num555\n";
+echo "$num6 $num66 $num666\n";
+echo "$num7 $num77 $num777\n";
+echo "$num8 $num88 $num888\n";
+echo "+------------------------------------------------------------+\n";
+
+ 
+
+
+?>
